9bc1501cbde1add07ba32efefe27f3eeff7b7ce7
zhouguilong <zhouguilong6@jd.com>
2025-03-06 16:49:48 +0800

    feat:手写mybatis框架

---
+++ mybatisMyself/src/test/java/com/taolong/mybatis_myself/MySelfMybatisTest.java
@@ -0,0 +1,23 @@
+package com.taolong.mybatis_myself;
+
+import org.junit.Test;
+
+import com.taolong.mybatis_myself.bean.TUser;
+import com.taolong.mybatis_myself.mapper.TUserMapper;
+import com.taolong.mybatis_myself.session.SqlSession;
+import com.taolong.mybatis_myself.session.SqlSessionFactory;
+
+public class MySelfMybatisTest {
+
+    /**
+     * 测试MyBatis的基本功能。
+     */
+	@Test
+	public void testMybatis() {
+		SqlSessionFactory factory = new SqlSessionFactory();
+		SqlSession session = factory.openSession();
+		TUserMapper userMapper = session.getMapper(TUserMapper.class);
+		TUser user = userMapper.selectByPrimaryKey(128);
+		System.out.println(user);
+	}
+}
